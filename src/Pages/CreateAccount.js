c33351dd911ee05057732e1d38d608078ee09778
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-06-29 21:16:59 -0400

    Added pages

---
+++ src/Pages/CreateAccount.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function CreateAccount() {
+  return (
+    <div>
+     <h1>Create Account Page</h1>
+    </div>
+  );
+}
+
+export default CreateAccount;
