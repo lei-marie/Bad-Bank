4ada4812604447e73ac323530cd728c3eeb34363
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-06-29 21:17:17 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import Home from './Pages/Home';
+import CreateAccount from './Pages/CreateAccount';
+import {BrowserRouter, Switch, Routes, Route} from 'react-router-dom';
+import Login from './Pages/Login';
+import AllData from './Pages/AllData';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     <h1>Bad Bank</h1>
+     <Nav />
+     <Routes>
+     <Route path="/" element={<Home />}></Route>
+     </Routes>
+     <Routes>
+     <Route path="/createaccount" element={<CreateAccount />}></Route>
+     </Routes>
+     <Routes>
+     <Route path="/login" element={<Login />}></Route>
+     </Routes>
+     <Routes>
+     <Route path="/alldata" element={<AllData />}></Route>
+     </Routes>
+     
     </div>
+    </BrowserRouter>
   );
 }
 
