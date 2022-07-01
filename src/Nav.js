ac6aca178667b4e530b341b9ef71f89c94e05ff9
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-07-01 12:26:13 -0400

    Updated

--- src/Nav.js
+++ src/Nav.js
@@ -2,28 +2,26 @@ import React from 'react';
 import './App.css';
 import { Link } from 'react-router-dom';
 
-function Nav() {
-  return (
-       
-        <nav className="navbar navbar-expand-lg bg-secondary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"> <h3>Bad Bank</h3></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
+ function Nav() {
+   return (
+    <nav className="navbar navbar-expand-lg bg-secondary">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+       <ul className="navbar-nav">
         <Link to="/">
-        <li  className="nav-link" aria-current="page">Home</li>
+         <li  className="nav-link" aria-current="page">Home</li>
         </Link>
         <Link to="/createaccount">
-        <li  className="nav-link" aria-current="page">Create Account</li>
+          <li  className="nav-link" aria-current="page">Create Account</li>
         </Link>
         <Link to="/login">
-        <li  className="nav-link" aria-current="page">Login</li>
+          <li  className="nav-link" aria-current="page">Login</li>
         </Link>
         <Link to="/alldata">
-        <li  className="nav-link" aria-current="page">AllData</li>
+          <li  className="nav-link" aria-current="page">All Data</li>
         </Link>
       </ul>
     </div>
@@ -32,5 +30,5 @@ function Nav() {
 
   );
 }
-<a className="nav-link active" aria-current="page" href="#">Home</a>
+
 export default Nav;
