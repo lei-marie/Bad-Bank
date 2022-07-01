54d7983fe3100ec3a7ceeac50d5b7906a6581c5e
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-07-01 12:25:45 -0400

    Added Bootstrap Card

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../App.css';
 
-function Home () {
+function Home(){
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    );
-}
+      <div className="card">
+      <div className="card-body">
+        <h5 className="card-title">Bad Bank</h5>
+        <p className="card-text">Welcome, to your trusty bank. Your money is always safe with us!</p>
+        <p className="card-text"><small className="text-muted">No Security, but that's okay</small></p>
+      </div>
+      <img src="https://th.bing.com/th/id/R.e44c1e185cef99ea7869bdc9f3a4bf6a?rik=HSJe4jSV%2fZCklw&pid=ImgRaw&r=0" className="card-img-top" alt="Image Front of bank" />
+      </div>
+    );  
+  }
 
-export default Home;
+ export default Home;
+
+ 
