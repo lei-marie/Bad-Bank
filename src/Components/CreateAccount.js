47b4dfe27f162607b0901b51f07297e8045d5237
Lei Marie Lafaele <llafaele.murphy@outlook.com>
2022-07-17 16:20:49 -0400

    updated  form

--- src/Components/CreateAccount.js
+++ src/Components/CreateAccount.js
@@ -4,11 +4,27 @@ import '../../src/App.css';
 function CreateAccount() {
 
 return (
-    <h1>Create Account</h1>
+    <div className="container2">
+        <div className="card bg-dark text-white" id="card2">
+            <img src="https://th.bing.com/th/id/R.03b0dc237a10ba037d3673c93ff16dac?rik=StFhKjrfR2%2bHFQ&pid=ImgRaw&r=0" className="card-img4" alt="mountainview lake" id="accountImage"/>
+            <div className="card-img-overlay">
+                <h1 className="card-title">Create an Account</h1>
+                <br/>
+                <br/>
+            <div className="mb-3 row">
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="exampleFormControlInput1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="password" />
+            </div>
+            </div> 
+            </div>
+        </div>
+    </div>
 );
 }
 
-export default CreateAccount;
-
-
- 
+export default CreateAccount; 
